0c99f1319fd6a06ca7213b5d78767d10f327e852
Mark Hamstra <hello@markhamstra.com>
2019-07-25 14:13:33 +0200

    List packages in the about page

--- core/components/commerce_mpdfwriter/src/Modules/mPDFWriter.php
+++ core/components/commerce_mpdfwriter/src/Modules/mPDFWriter.php
@@ -1,5 +1,8 @@
 <?php
 namespace modmore\Commerce_mPDFWriter\Modules;
+use modmore\Commerce\Admin\Configuration\About\ComposerPackages;
+use modmore\Commerce\Admin\Sections\SimpleSection;
+use modmore\Commerce\Events\Admin\PageEvent;
 use modmore\Commerce\Events\PDFWriter;
 use modmore\Commerce\Modules\BaseModule;
 use modmore\Commerce_mPDFWriter\Writer;
@@ -32,13 +35,30 @@ class mPDFWriter extends BaseModule {
         $this->adapter->loadLexicon('commerce_mpdfwriter:default');
 
         // Load PDF Writer
-        $dispatcher->addListener(\Commerce::EVENT_GET_PDF_WRITER, array($this, 'getPDFWriter'));
+        $dispatcher->addListener(\Commerce::EVENT_GET_PDF_WRITER, [$this, 'getPDFWriter']);
+        $dispatcher->addListener(\Commerce::EVENT_DASHBOARD_LOAD_ABOUT, [$this, 'addLibrariesToAbout']);
     }
 
     public function getPDFWriter(PDFWriter $event)
     {
         $instance = new Writer();
         $event->addWriter($instance);
+    }
+
+    public function addLibrariesToAbout(PageEvent $event)
+    {
+        $lockFile = dirname(dirname(__DIR__)) . '/composer.lock';
+        if (file_exists($lockFile)) {
+            $section = new SimpleSection($this->commerce);
+            $section->addWidget(new ComposerPackages($this->commerce, [
+                'lockFile' => $lockFile,
+                'heading' => $this->adapter->lexicon('commerce.about.open_source_libraries') . ' - ' . $this->adapter->lexicon('commerce_mpdfwriter'),
+                'introduction' => '', // Could add information about how libraries are used, if you'd like
+            ]));
+
+            $about = $event->getPage();
+            $about->addSection($section);
+        }
     }
 
     public function getModuleConfiguration(\comModule $module)
