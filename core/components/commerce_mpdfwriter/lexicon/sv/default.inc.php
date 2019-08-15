7a30672a7c8b9423005b84cc74229b76eb0b5ea2
Mark Hamstra <hello@markhamstra.com>
2019-08-15 15:39:37 +0200

    Sync translations

---
+++ core/components/commerce_mpdfwriter/lexicon/sv/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_mpdfwriter'] = 'PDF Writer: mPDFWriter';
+$_lang['commerce_mpdfwriter.module_description'] = 'Adds a PDF Writer that Commerce can use to generate PDFs (like invoices), based on the mPDF 8.x library.';
