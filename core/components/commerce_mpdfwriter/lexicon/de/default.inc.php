28567516a377a24170920a5c9c183eae5264b480
Mark Hamstra <hello@markhamstra.com>
2020-04-15 12:51:08 +0200

    Translation sync

--- core/components/commerce_mpdfwriter/lexicon/de/default.inc.php
+++ core/components/commerce_mpdfwriter/lexicon/de/default.inc.php
@@ -1,4 +1,4 @@
 <?php
 
 $_lang['commerce_mpdfwriter'] = 'PDF Writer: mPDFWriter';
-$_lang['commerce_mpdfwriter.module_description'] = 'Adds a PDF Writer that Commerce can use to generate PDFs (like invoices), based on the mPDF 8.x library.';
+$_lang['commerce_mpdfwriter.module_description'] = 'Fügt einen PDF Writer hinzu, mit dem Commerce PDF Dateien (z.B. Rechnungen) mittels der mPDF 8.x-Bibliothek generieren kann.';
